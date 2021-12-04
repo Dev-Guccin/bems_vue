ade0f4503e2a8b8ed1e7592ba91bed518feeaef3
wodhks08 <ghks2047@naver.com>
2021-12-04 12:03:35 +0900

    feat: backend modify response json

--- backend/routes/settings.js
+++ backend/routes/settings.js
@@ -8,30 +8,45 @@ let router = express.Router()
 router.get('/restart_only/:module', function (req, res, next) {
   console.log('restart_only_module : ', req.params.module)
   Handler.restart_only(req.params.module)
-  res.send('respond with a resource')
+  res.send({
+    success: true,
+    message: `restart ${req.params.module} module`,
+  })
 })
 router.get('/stop_only/:module', function (req, res, next) {
   console.log('stop_only_module : ', req.params.module)
   Handler.stop_only(req.params.module)
-  res.send('respond with a resource')
+  res.send({
+    success: true,
+    message: `stop ${req.params.module} module`,
+  })
 })
 router.get('/stop_all', function (req, res, next) {
   console.log('stop_all')
   Handler.stop_all()
-  res.send('respond with a resource')
+  res.send({
+    success: true,
+    message: "stop all module"
+  })
 })
 router.get('/module_check', async function (req, res, next) {
   console.log('module_check')
   let checklist = await Handler.module_check()
-  res.send(checklist)
+  res.send({
+    success: true,
+    checklist:checklist
+  })
 })
 
 router.get('/module_log_check/:module/:type', async function (req, res, next) {
   console.log('module_log_check')
   let module = req.params.module
   let type = req.params.type
-  let log_data = await Handler.get_logfile(module, type)
-  res.send(log_data)
+  let logData = await Handler.get_logfile(module, type)
+  res.send({
+    success: true,
+    logData: logData,
+  })
 })
 
 module.exports = router
