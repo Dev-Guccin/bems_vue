13dee55097f4f4e7d12e33a46e406764bf501651
sungyeon-0975 <sustain98@naver.com>
2021-12-19 01:23:10 +0900

    다른 모듈 재시작시 batch도 재시작

--- backend/routes/settings.js
+++ backend/routes/settings.js
@@ -5,12 +5,14 @@ let router = express.Router()
 // modbus 제어, bacnet제어, database제어
 // 3가지 모두 pm2를 사용하여 제어를 진행한다.
 //api/settings/restart_only/modbus
-router.get('/restart_only/:module', function (req, res, next) {
+router.get('/restart_only/:module', async function (req, res, next) {
   console.log('restart_only_module : ', req.params.module)
-  Handler.stop_only('batch')
-  Handler.restart_only(req.params.module)
-  Handler.restart_only('batch')
   res.send('respond with a resource')
+  await Handler.restart_only(req.params.module)
+  if (!(req.params.module == 'batch')){
+    await Handler.restart_only('batch')
+  }
+  
 })
 router.get('/stop_only/:module', function (req, res, next) {
   console.log('stop_only_module : ', req.params.module)
